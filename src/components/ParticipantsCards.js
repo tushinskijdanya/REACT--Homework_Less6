45e619aa3a3e26ec37d6fca316a002f58c11ff3d
Daniel <tushinskij.danya@gmail.com>
2023-06-10 17:57:12 +0300

    intermediate end

--- src/components/ParticipantsCards.js
+++ src/components/ParticipantsCards.js
@@ -2,16 +2,16 @@ function ParticipantsCards (props) {
     console.log(props)
     return(
         <div className="part-card">
-            <p className="part-number">{(props.card.idx + 1)}.</p>
+            <p className="part-number">Participant №{props.idx + 1}.</p>
             <div className="pard-card_content">
                 <div className="part-card_main">
                     <p className="nickname">{props.card.nickname}</p>
-                    <p>age: {props.card.age}</p>
+                    <div className="years-gender_block">
+                        <p className="years">{props.card.age} years</p>
+                        <p className="gender">{(props.card.gender !== '') ? `(${props.card.gender})` : ''}</p>
+                    </div>
                 </div>
-                <div className="part-card_gender-city">
-                    <p>{props.card.gender}</p>
-                    <p>{props.card.city}</p>
-                </div>
+                <p className="city">{props.card.city}</p>
             </div>
         </div>
     )
